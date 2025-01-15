4d918844dd3230bb07bff19730113131e14db29a
Samira Keshavarz <samira.keshavarz@rewe-digital.com>
2025-01-15 09:02:11 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {DashboardComponent} from './books/dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [DashboardComponent],
+  templateUrl: './app.component.html', // über den binden wir die HTML-Datei
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Book Rating!';
+}
