b6daa62bf22ef61c8c96ba2eef39ec1abe646373
Samira Keshavarz <s.keshavarz@gmx.de>
2025-01-20 11:42:56 +0100

    implement creation Formular model

--- src/app/books/book-create/book-create.component.ts
+++ src/app/books/book-create/book-create.component.ts
@@ -1,11 +1,19 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './book-create.component.html',
   styleUrl: './book-create.component.scss'
 })
 export class BookCreateComponent {
 
+  bookForm = new FormGroup({
+    isbn: new FormControl('', {nonNullable: true, validators: []}),
+    title: new FormControl('', {nonNullable: true, validators: []}),
+    description: new FormControl('', {nonNullable: true, validators: []}),
+    rating: new FormControl(5, {nonNullable: true, validators: []}),
+    price: new FormControl(0, {nonNullable: true, validators: []})
+  });
 }
