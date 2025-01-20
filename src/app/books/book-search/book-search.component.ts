479db9d27cbffe490891201676e25d6e46e9f738
Samira Keshavarz <s.keshavarz@gmx.de>
2025-01-20 11:05:17 +0100

    implement navigation

---
+++ src/app/books/book-search/book-search.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-book-search',
+  imports: [],
+  templateUrl: './book-search.component.html',
+  styleUrl: './book-search.component.scss'
+})
+export class BookSearchComponent {
+
+}
