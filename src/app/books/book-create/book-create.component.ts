62c650fabfb01e7331e0e590e4efb69c976bc375
Samira Keshavarz <s.keshavarz@gmx.de>
2025-01-20 12:31:19 +0100

    implement validators, and show message depending on validation failure

--- src/app/books/book-create/book-create.component.ts
+++ src/app/books/book-create/book-create.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
@@ -10,10 +10,35 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 export class BookCreateComponent {
 
   bookForm = new FormGroup({
-    isbn: new FormControl('', {nonNullable: true, validators: []}),
-    title: new FormControl('', {nonNullable: true, validators: []}),
+    isbn: new FormControl('', {nonNullable: true, validators: [
+      Validators.required,
+        Validators.minLength(7),
+        Validators.maxLength(15),
+        Validators.pattern(/^[0-9]*$/)
+      ]}),
+    title: new FormControl('', {nonNullable: true, validators: [
+      Validators.required,
+        Validators.maxLength(100)
+
+      ]}),
     description: new FormControl('', {nonNullable: true, validators: []}),
-    rating: new FormControl(5, {nonNullable: true, validators: []}),
-    price: new FormControl(0, {nonNullable: true, validators: []})
+    rating: new FormControl(5, {nonNullable: true, validators: [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(5),
+      ]}),
+    price: new FormControl(0, {nonNullable: true, validators: [
+      Validators.required,
+        Validators.min(0)
+      ]})
   });
+
+  isInvalid(control: FormControl): boolean {
+    return control.invalid && control.touched
+  }
+
+  hasError(control: FormControl, errorCode: string) : boolean {
+    // hat dieses Control diesen bestimmten Fehler?
+    return control.hasError(errorCode) && control.touched
+  }
 }
