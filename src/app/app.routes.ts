b2d1830463071246a337cd5610614207d10ec2bb
Samira Keshavarz <s.keshavarz@gmx.de>
2025-01-20 10:48:04 +0100

    implement lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import {Routes} from '@angular/router';
-import {booksRoutes} from './books/books.routes';
 
 export const routes: Routes = [
   // bei Weiterleitung vom leeren Pfad immer mit pathMatch: full
   {path: '', redirectTo: 'books', pathMatch: 'full'},
-  ...booksRoutes,
+
+  // Eager loading würde so aussehen
+  //...booksRoutes,
   // weitere Beispiele:
   // ...adminRoutes,
   // ...userRoutes
+
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books.routes').then(m => m.booksRoutes) // dynamisches import statement
+  }
 ];
 
--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -12,6 +12,8 @@ import {BookComponent} from '../book/book.component';
 })
 export class BookDetailsComponent {
 
+  // einfache properties kann man einfach optional machen, aber signals nicht.
+  // Daher nutzen wir undefined hier
   readonly book = signal<Book | undefined>(undefined)
 
   private route = inject(ActivatedRoute);
@@ -20,12 +22,15 @@ export class BookDetailsComponent {
   constructor() {
     // PULL basierten weg, bei den weird der isbn sich nicht aktualisieren, sondern einmalig geladen
     // ein großes Object in den alles steht was zu unsere Route gehört.
+    // das nutzen wir lieber nicht weil wenn isbn sich ändert, wird sich die Seite nicht aktualisieren.
     // const isbn = this.route.snapshot.paramMap.get('isbn'); // aus den path: '/books/:isbn
     // console.log(isbn);
 
     // PUSH basierten weg: bei den Fall jede Änderungen an isbn wird mitgeteilt
+    // das wird jedes Mal durchlaufen sobald isbn (param) sich ändert.
+    // mit fragment können wir auf #blabla in der URI zugreifen.
     this.route.paramMap.subscribe(params => {
-      const isbn = params.get('isbn') // oder ! benutzen anstatt if condition
+      const isbn = params.get('isbn') // oder ! benutzen anstatt if condition: params.get('isbn')!
       if (isbn !== null) {
         this.bookStoreService.getSingle(isbn!).subscribe(
         book => {
