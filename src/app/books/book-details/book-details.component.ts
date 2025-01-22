4aaee738409fbdaa312872c814a8d1dacf33821b
Samira Keshavarz <s.keshavarz@gmx.de>
2025-01-22 10:54:41 +0100

    playing around with RxJs, Async Pipe and Signals

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,8 +1,9 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {ActivatedRoute, RouterLink} from '@angular/router';
 import {BookStoreService} from '../shared/book-store.service';
-import {Book} from '../shared/book';
 import {BookComponent} from '../book/book.component';
+import {map, switchMap} from 'rxjs';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-book-details',
@@ -14,10 +15,22 @@ export class BookDetailsComponent {
 
   // einfache properties kann man einfach optional machen, aber signals nicht.
   // Daher nutzen wir undefined hier
-  readonly book = signal<Book | undefined>(undefined)
+  // Für Lösung mit alles andere al toSignal()
+  //readonly book = signal<Book | undefined>(undefined)
 
   private route = inject(ActivatedRoute);
   private bookStoreService = inject(BookStoreService);
+
+  // Lösung mit async Pipe. Async kümmert sich um unsubscribe.
+  // book$ = this.route.paramMap.pipe(
+  //   map(param => param.get('isbn')!),
+  //   switchMap(isbn => this.bookStoreService.getSingle(isbn))
+  // );
+
+  book2$ = toSignal(this.route.paramMap.pipe(
+    map(param => param.get('isbn')!),
+    switchMap(isbn => this.bookStoreService.getSingle(isbn))
+  ))
 
   constructor() {
     // PULL basierten weg, bei den weird der isbn sich nicht aktualisieren, sondern einmalig geladen
@@ -29,14 +42,22 @@ export class BookDetailsComponent {
     // PUSH basierten weg: bei den Fall jede Änderungen an isbn wird mitgeteilt
     // das wird jedes Mal durchlaufen sobald isbn (param) sich ändert.
     // mit fragment können wir auf #blabla in der URI zugreifen.
-    this.route.paramMap.subscribe(params => {
-      const isbn = params.get('isbn') // oder ! benutzen anstatt if condition: params.get('isbn')!
-      if (isbn !== null) {
-        this.bookStoreService.getSingle(isbn!).subscribe(
-        book => {
-          this.book.set(book)
-        })
-      }
-    })
+    // this.route.paramMap.subscribe(params => {
+    //   const isbn = params.get('isbn') // oder ! benutzen anstatt if condition: params.get('isbn')!
+    //   if (isbn !== null) {
+    //     this.bookStoreService.getSingle(isbn!).subscribe(
+    //     book => {
+    //       this.book.set(book)
+    //     })
+    //   }
+    // })
+
+    // Push basiert: Lösung mit rxjs
+    // this.route.paramMap.pipe(
+    //   map(param => param.get('isbn')!),
+    //   switchMap(isbn => this.bookStoreService.getSingle(isbn))
+    // )
+    // .subscribe(book => {this.book.set(book)})
+
   }
 }
--- src/app/books/dashboard/dashboard.component.ts
+++ src/app/books/dashboard/dashboard.component.ts
@@ -1,50 +1,69 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject, OnDestroy, signal} from '@angular/core';
 import {Book} from '../shared/book';
 import {BookComponent} from '../book/book.component';
 import {BookRatingService} from '../shared/book-rating.service';
 import {BookStoreService} from '../shared/book-store.service';
-import {DatePipe} from '@angular/common';
-import {Subscription, timer} from 'rxjs';
+import {AsyncPipe, DatePipe} from '@angular/common';
+import {map, startWith, Subscription, timer} from 'rxjs';
+import {takeUntilDestroyed, toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [BookComponent, DatePipe],
+  imports: [BookComponent, DatePipe, AsyncPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnDestroy {
 
   books = signal<Book[]>([]);
 
   // Lösung 1
-  currentDate1 = signal<Date>(new Date())
+  currentDate1 = signal(Date.now())
   private readonly intervalId;
 
   // Lösung 2
-  currentDate2 = signal<Date>(new Date())
-  timeCounter = timer(0, 1000);
-  timerSubscription : Subscription
+  currentDate2 = signal(Date.now())
+  timerSubscription: Subscription
+
+  currentDate3 = signal(Date.now())
+
+
+  // Lösung 4: mit Angular Signals
+  readonly currentDate4 = toSignal(
+    timer(0, 1000)
+      .pipe(map(() => Date.now()), startWith(Date.now())),
+    {requireSync: true} // dazu aus folien nach blättern
+  )
 
   #rs = inject(BookRatingService);
-  private bookStoreService = inject(BookStoreService);
+  currentDate5= timer(0, 1000).pipe(map( _ => Date.now()))
 
+  private bookStoreService = inject(BookStoreService);
 
   constructor() {
     this.bookStoreService.getAll().subscribe(books => {
       this.books.set(books)
     })
 
-    // Lösung 1
+    // Lösung 1 continue
     this.intervalId = setInterval(() => {
-      this.currentDate1.set(new Date())
+      this.currentDate1.set(Date.now())
       console.log('COUNTER 1', this.currentDate1())
     }, 1000);
 
     // Lösung 2
-    this.timerSubscription = this.timeCounter.subscribe(() => {
-      this.currentDate2.set(new Date())
+    this.timerSubscription = timer(0, 1000).subscribe(() => {
+      this.currentDate2.set(Date.now())
       console.log('COUNTER 2', this.currentDate2())
     });
+
+    // Lösung 3 continue : mit RxJs
+    timer(0, 1000)
+      .pipe(
+        map(() => Date.now()),
+        takeUntilDestroyed()
+      ).subscribe(ts => this.currentDate3.set(ts))
+
 
   }
 
@@ -78,8 +97,8 @@ export class DashboardComponent {
   }
 
   // eine einfachere Variation von delete mit nativen confirm() funktion und lokalen update der Liste
-  doDelete2(book: Book){
-    if (!confirm('Buch löschem')){
+  doDelete2(book: Book) {
+    if (!confirm('Buch löschem')) {
       return;
     }
     this.bookStoreService.delete(book.isbn).subscribe(() => {
@@ -87,11 +106,12 @@ export class DashboardComponent {
     })
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.timerSubscription.unsubscribe()
     clearInterval(this.intervalId)
+    console.log('Destroy')
   }
 
-    // wenn man mit computed Signal das lesen würde, aber das wäre zu over-engineered.
-    // booksCount = computed(() => this.books.length)
+  // wenn man mit computed Signal das lesen würde, aber das wäre zu over-engineered.
+  // booksCount = computed(() => this.books.length)
 }
